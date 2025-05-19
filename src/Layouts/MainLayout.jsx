e86f98f9f66bb9936f8f6361202f5de90387952f
arafat22184 <123alarafat@gmail.com>
2025-05-20 02:13:36 +0600

    added navbar

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <h1 className="text-5xl">Main layout</h1>
     </div>
   );
