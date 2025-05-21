def4fb5710015b148e05f0c16c8fa546a5d00821
arafat22184 <123alarafat@gmail.com>
2025-05-21 23:02:53 +0600

    added theme to My Groups page

--- src/Components/UserGroups.jsx
+++ src/Components/UserGroups.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import UserGroup from "./UserGroup";
+import { AuthContext } from "../Provider/AuthProvider";
+import {
+  FaUsers,
+  FaMapMarkerAlt,
+  FaCalendarAlt,
+  FaListUl,
+  FaLayerGroup,
+  FaCog,
+} from "react-icons/fa";
 
 const UserGroups = ({ userGroups, setUserGroups }) => {
+  const { theme } = useContext(AuthContext);
+
+  const tableHeadStyle = `p-3 text-sm font-semibold ${
+    theme === "light" ? "bg-gray-200 text-gray-800" : "bg-gray-700 text-white"
+  }`;
+  const tableBodyStyle =
+    theme === "light" ? "bg-white text-gray-900" : "bg-gray-800 text-white";
+
   return (
     <div className="overflow-x-auto">
       <table className="w-full text-left border-collapse text-sm">
-        <thead className="bg-gray-700 text-white">
+        <thead>
           <tr>
-            <th className="p-3">Sl.</th>
-            <th className="p-3">Group Name</th>
-            <th className="p-3">Category</th>
-            <th className="p-3">Members</th>
-            <th className="p-3">Start Date</th>
-            <th className="p-3">Location</th>
-            <th className="p-3 text-center">Actions</th>
+            <th className={tableHeadStyle}>
+              <FaListUl className="inline-block mr-1" /> Sl.
+            </th>
+            <th className={tableHeadStyle}>
+              <FaLayerGroup className="inline-block mr-1" /> Group Name
+            </th>
+            <th className={tableHeadStyle}>
+              <FaListUl className="inline-block mr-1" /> Category
+            </th>
+            <th className={tableHeadStyle}>
+              <FaUsers className="inline-block mr-1" /> Members
+            </th>
+            <th className={tableHeadStyle}>
+              <FaCalendarAlt className="inline-block mr-1" /> Start Date
+            </th>
+            <th className={tableHeadStyle}>
+              <FaMapMarkerAlt className="inline-block mr-1" /> Location
+            </th>
+            <th className={`${tableHeadStyle} text-center`}>
+              <FaCog className="inline-block mr-1" /> Actions
+            </th>
           </tr>
         </thead>
-        <tbody className="bg-gray-800 text-white">
+        <tbody className={tableBodyStyle}>
           {userGroups.map((group, i) => (
             <UserGroup
               key={group._id}
@@ -24,7 +55,7 @@ const UserGroups = ({ userGroups, setUserGroups }) => {
               setUserGroups={setUserGroups}
               userGroups={userGroups}
               group={group}
-            ></UserGroup>
+            />
           ))}
         </tbody>
       </table>
