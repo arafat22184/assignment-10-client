3d42bd2c7e4b3f1f61117db6cc93af1468028aed
arafat22184 <123alarafat@gmail.com>
2025-06-26 04:11:42 +0600

    added button to banner section

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,6 +3,7 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { Link } from "react-router";
 
 const Banner = () => {
   return (
@@ -18,55 +19,79 @@ const Banner = () => {
       >
         <SwiperSlide>
           <div className=" bg-[url('https://i.ibb.co.com/JW61PbKz/DSC-0061-1.jpg')] bg-cover bg-center h-[70vh] flex items-center justify-center text-white px-4">
-            <div className="bg-black/60 p-6 rounded-xl max-w-3xl text-center">
-              <h2 className="text-4xl md:text-5xl font-bold mb-3">
+            <div className="bg-black/60 p-6 rounded-xl max-w-3xl text-center space-y-3">
+              <h2 className="text-4xl md:text-5xl font-bold ">
                 Discover Your Passion
               </h2>
               <p className="text-lg">
                 Join hobby groups in your area and meet people who love what you
                 love.
               </p>
+              <Link
+                to={"/groups"}
+                className="px-3 py-2 rounded-lg hover:bg-indigo-700 bg-indigo-600"
+              >
+                Explore Groups
+              </Link>
             </div>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
           <div className="bg-[url('https://i.ibb.co.com/20X8fgZ2/united.jpg')] bg-cover bg-center h-[70vh] flex items-center justify-center text-white px-4">
-            <div className="bg-black/60 p-6 rounded-xl max-w-3xl text-center">
-              <h2 className="text-4xl md:text-5xl font-bold mb-3">
+            <div className="bg-black/60 p-6 rounded-xl max-w-3xl text-center space-y-3">
+              <h2 className="text-4xl md:text-5xl font-bold ">
                 Create Your Own Group
               </h2>
               <p className="text-lg">
                 Start something new and invite others to grow your community.
               </p>
+              <Link
+                to={"/groups"}
+                className="px-3 py-2 rounded-lg hover:bg-indigo-700 bg-indigo-600"
+              >
+                Explore Groups
+              </Link>
             </div>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
           <div className="bg-[url('https://i.ibb.co.com/NdWLt5B7/mountain-Man.jpg')] bg-cover bg-center h-[70vh] flex items-center justify-center text-white px-4">
-            <div className="bg-black/60 p-6 rounded-xl max-w-3xl text-center">
-              <h2 className="text-4xl md:text-5xl font-bold mb-3">
+            <div className="bg-black/60 p-6 rounded-xl max-w-3xl text-center space-y-3">
+              <h2 className="text-4xl md:text-5xl font-bold ">
                 Explore Diverse Hobbies
               </h2>
               <p className="text-lg">
                 From painting to hiking, find your tribe and stay inspired every
                 week.
               </p>
+              <Link
+                to={"/groups"}
+                className="px-3 py-2 rounded-lg hover:bg-indigo-700 bg-indigo-600"
+              >
+                Explore Groups
+              </Link>
             </div>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
           <div className="bg-[url('https://i.ibb.co.com/bMzmKNTf/group.jpg')] bg-cover bg-center h-[70vh] flex items-center justify-center text-white px-4">
-            <div className="bg-black/60 p-6 rounded-xl max-w-3xl text-center">
-              <h2 className="text-4xl md:text-5xl font-bold mb-3">
+            <div className="bg-black/60 p-6 rounded-xl max-w-3xl text-center space-y-3">
+              <h2 className="text-4xl md:text-5xl font-bold ">
                 Connect. Create. Celebrate.
               </h2>
               <p className="text-lg">
                 Be part of meaningful experiences. HobbyHub brings people
                 together.
               </p>
+              <Link
+                to={"/groups"}
+                className="px-3 py-2 rounded-lg hover:bg-indigo-700 bg-indigo-600"
+              >
+                Explore Groups
+              </Link>
             </div>
           </div>
         </SwiperSlide>
